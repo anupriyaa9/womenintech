8c94c476ccea555e0b9cd81d19f3527409380afc
Anupriya Asok <84090655+anupriyaa9@users.noreply.github.com>
2022-02-10 21:49:31 +0530

    update changes

--- calculator.cpp
+++ calculator.cpp
@@ -11,16 +11,16 @@ int main()
     cin >> num1 >> num2;
     switch(opr){
         case '1': 
-                cout <<"\nResult: " num1 << " + " << num2 << " = " << num1+num2;
+                cout <<"\nResult: "<< num1 << " + " << num2 << " = " << num1+num2;
                 break;
         case '2':
-                cout <<"\nResult: " num1 << " - " << num2 << " = " << num1-num2;
+                cout <<"\nResult: "<< num1 << " - " << num2 << " = " << num1-num2;
                 break;
         case '3':
-                cout <<"\nResult: " num1 << " * " << num2 << " = " << num1*num2;
+                cout <<"\nResult: "<< num1 << " * " << num2 << " = " << num1*num2;
                 break;
         case '4':
-                cout <<"\nResult: " num1 << " / " << num2 << " = " << num1/num2;
+                cout <<"\nResult: "<< num1 << " / " << num2 << " = " << num1/num2;
                 break;
         default: 
                 cout<<"ERROR! Please select a valid operation.";
